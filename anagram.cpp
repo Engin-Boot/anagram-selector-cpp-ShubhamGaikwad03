85c8b7dc062eb615e0f172b9b0d56c901dfa1d6c
Shubham <Shubham.gaikwad88.15@gmail.com>
2020-09-05 23:51:14 +0530

    Anagram Update

--- anagram.cpp
+++ anagram.cpp
@@ -2,11 +2,21 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+string removeSpaces(string str)
+{
+    transform(str.begin(), str.end(), str.begin(), ::toupper);
+    str.erase(remove(str.begin(),str.end(),' '),str.end());
+    return str;
+}
+
 bool Anagram::WordPairIsAnagram(const std::string& word1, const std::string& word2) {
     
     string word1_str=word1;
     string word2_str=word2;
-
+    
+    word1_str=removeSpaces(word1_str);
+    word2_str=removeSpaces(word2_str);
+    
     int word1_len=word1_str.length();
     int word2_len=word2_str.length();
 
