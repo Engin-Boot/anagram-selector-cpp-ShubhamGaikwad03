a11d1f4f1a7077fdced0f10ae1da631c50bd5bd7
Shubham <Shubham.gaikwad88.15@gmail.com>
2020-09-06 00:15:38 +0530

    Anagram Test Case

--- anagram-test.cpp
+++ anagram-test.cpp
@@ -3,21 +3,24 @@
 #include "catch.hpp"
 #include "anagram.h"
 
-TEST_CASE("recognizes shuffled alphabets as anagrams") {
+TEST_CASE("recognizes shuffled alphabets as anagrams")
+{
     REQUIRE(Anagram::WordPairIsAnagram("restful", "fluster") == true);
     REQUIRE(Anagram::WordPairIsAnagram("forty five", "over fifty") == true);
 }
 
-TEST_CASE("reports non-anagrams") {
+TEST_CASE("reports non-anagrams") 
+{
     REQUIRE(Anagram::WordPairIsAnagram("something", "else") == false);
 }
 
-TEST_CASE("recognizes anagrams when there are case- and space- differences") {
+TEST_CASE("recognizes anagrams when there are case- and space- differences") 
+{
     REQUIRE(Anagram::WordPairIsAnagram("New York Times", "monkeys write")
         == true);
 }
-
-TEST_CASE("selects anagrams of a word") {
+TEST_CASE("selects anagrams of a word")
+{
     auto selection = Anagram::SelectAnagrams("master",
         {"stream", "something", "maters"});
     REQUIRE(selection == std::vector<std::string>{"stream", "maters"});
